438d0181ceff099f6ed9e2460726260354d6437f
eunmo <eunmo@cs.stanford.edu>
2019-04-07 21:09:43 +0900

    more cleanup for kleague

--- cron/semi-hourly.sh
+++ cron/semi-hourly.sh
@@ -1,4 +1,5 @@
 curl "localhost:3050/api/match/fetch-season/2019"
+curl "localhost:3050/api/match/cleanup"
 curl "localhost:3050/api/league/update-all/2019"
 curl "localhost:3050/api/cup/fetch/ongoing/2019"
 curl "localhost:3050/api/qual/fetch/2019"
