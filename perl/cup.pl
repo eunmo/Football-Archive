e0d240bd7b1b7eeb2ebef7d948de1f766e518a91
eunmo <eunmo@cs.stanford.edu>
2019-07-13 22:58:18 +0900

    handle bye (by excluding lower rounds.. for now)

--- perl/cup.pl
+++ perl/cup.pl
@@ -15,69 +15,71 @@ my $date;
 my $json = "[";
 
 for my $tr ($dom->find('div[class="portfolio"] div[class="box"] tr')->each) {
-	my $round_th = $tr->find('th');
+  my $round_th = $tr->find('th');
 
-	if ($round_th->size) {
-		$round = $round_th->first->all_text;
-		$round =~ s/\.//g;
-		$round =~ s/2nd/2/g;
-		$round =~ s/3rd/3/g;
+  if ($round_th->size) {
+    $round = $round_th->first->all_text;
+    $round =~ s/\.//g;
+    $round =~ s/2nd/2/g;
+    $round =~ s/3rd/3/g;
 
-		$match_count = 0;
+    $match_count = 0;
+  } else {
+    my $td_col = $tr->find('td');	
+
+    if ($td_col->size) {
+      if ($td_col->[0]->all_text ne '') {
+	$date = $td_col->[0]->all_text;
+
+	# convert: dd/mm/yyyy -> mm/dd/yyyy
+	$date =~ /(\d+)\/(\d+)\/(\d+)/;
+	$date = "$2/$1/$3";
+      }
+
+      next if $td_col->[2]->find('a')->size == 0;
+
+      my $l = trim($td_col->[2]->all_text);
+      my $r = trim($td_col->[4]->all_text);
+
+      if ($cup eq "/all_matches/chn-fa-cup-2017/" && $round eq "3 Round" &&
+	$l eq "Qingdao Huanghai" && $r eq "Guangzhou Evergrande") {
+	$r = "Guangzhou R&F";
+      }
+      
+      next if $cup eq "/all_matches/chn-fa-cup-2019_2/" && $l eq "Wuhan Three Towns" && $r eq "Jinan Dayou";
+
+      my $score_td = $td_col->[5];
+      my $score = '-:-';
+      my $pk = '';
+
+      if ($score_td->find('span')->size == 0 || $score_td->find('span')->first->attr('style') !~ '^color') {
+	$score = trim($score_td->all_text);
+
+	if ($score =~ /(\d+:\d+).*(\d:\d+)\) pso/) {
+	  $score = $2;
+	  $pk = $1;
 	} else {
-		my $td_col = $tr->find('td');	
-
-		if ($td_col->size) {
-			if ($td_col->[0]->all_text ne '') {
-				$date = $td_col->[0]->all_text;
-
-				# convert: dd/mm/yyyy -> mm/dd/yyyy
-				$date =~ /(\d+)\/(\d+)\/(\d+)/;
-				$date = "$2/$1/$3";
-			}
-
-			next if $td_col->[2]->find('a')->size == 0;
-
-			my $l = trim($td_col->[2]->all_text);
-			my $r = trim($td_col->[4]->all_text);
-
-			if ($cup eq "/all_matches/chn-fa-cup-2017/" && $round eq "3 Round" &&
-					$l eq "Qingdao Huanghai" && $r eq "Guangzhou Evergrande") {
-				$r = "Guangzhou R&F";
-			}
-
-			my $score_td = $td_col->[5];
-			my $score = '-:-';
-			my $pk = '';
-
-			if ($score_td->find('span')->size == 0 || $score_td->find('span')->first->attr('style') !~ '^color') {
-				$score = trim($score_td->all_text);
-
-				if ($score =~ /(\d+:\d+).*(\d:\d+)\) pso/) {
-					$score = $2;
-					$pk = $1;
-				} else {
-					$score =~ s/\s\(.*//;
-				}
-			}
-
-			next if $score =~ "resch";
-			
-			my $url = getUrl($score_td);
-
-			if ($match_count == 0) {
-				$json .= "]}\n," if $round_count++;
-				$json .= "{\"name\": \"$round\", \"matches\": [\n";
-			}
-			
-			$json .= ",\n" if $match_count++;
-			$json .= "{\"date\": \"$date\", \"l\": \"$l\", \"r\": \"$r\"";
-			$json .= ", \"score\": \"$score\"" if $score ne '-:-';
-			$json .= ", \"pk\": \"$pk\"" if $pk ne '';
-			$json .= ", \"url\": \"$url\"" if $url ne '';
-			$json .= "}";
-		}
+	  $score =~ s/\s\(.*//;
 	}
+      }
+
+      next if $score =~ "resch";
+
+      my $url = getUrl($score_td);
+
+      if ($match_count == 0) {
+	$json .= "]}\n," if $round_count++;
+	$json .= "{\"name\": \"$round\", \"matches\": [\n";
+      }
+
+      $json .= ",\n" if $match_count++;
+      $json .= "{\"date\": \"$date\", \"l\": \"$l\", \"r\": \"$r\"";
+      $json .= ", \"score\": \"$score\"" if $score ne '-:-';
+      $json .= ", \"pk\": \"$pk\"" if $pk ne '';
+      $json .= ", \"url\": \"$url\"" if $url ne '';
+      $json .= "}";
+    }
+  }
 }
 
 $json .= "]}" if $round_count;
@@ -88,22 +90,22 @@ print "\n\n\n\n\n";
 
 sub getUrl($)
 {
-	my $td_col = shift;
-			
-	my $url_link = $td_col->find('a');
+  my $td_col = shift;
 
-	if ($url_link->size == 0) {
-		return (trim($td_col->all_text) eq 'dnp') ? 'dnp' : '';
-	}
-	return 'dnp' if trim($url_link->[0]->all_text) eq 'dnp';
+  my $url_link = $td_col->find('a');
 
-	my $url = $url_link->[0]->attr('href');
+  if ($url_link->size == 0) {
+    return (trim($td_col->all_text) eq 'dnp') ? 'dnp' : '';
+  }
+  return 'dnp' if trim($url_link->[0]->all_text) eq 'dnp';
 
-	$url =~ s/liveticker\/$//;
-	$url =~ s/^\/report//;
-	$url =~ s/^\/|\/$//g;
+  my $url = $url_link->[0]->attr('href');
 
-	return $url;
+  $url =~ s/liveticker\/$//;
+  $url =~ s/^\/report//;
+  $url =~ s/^\/|\/$//g;
+
+  return $url;
 }
 
 sub trim($)
