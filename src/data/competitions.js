3a653a42343f68a9647b4a16e41c651d4513f924
eunmo <eunmo@cs.stanford.edu>
2019-07-18 23:29:44 +0900

    update packages

--- src/data/competitions.js
+++ src/data/competitions.js
@@ -125,8 +125,8 @@ const competitions = {
     sh: 'WCQ',
     type: 'Q',
     group: 0,
-    times: [2015, 2016, 2017],
-    spans: [2017]
+    times: [2015, 2016, 2017, 2019],
+    spans: [2017, 2019]
   },
   'EURO Qualifiers': {
     order: 5,
